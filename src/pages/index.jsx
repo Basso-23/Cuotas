00b0543d7fb588dabf6aa8ce607d76304274cfa9
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-04-20 17:44:54 -0500

    commit message

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <div>Hola</div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
